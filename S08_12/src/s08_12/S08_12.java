1b7c5cc9bef164006d7db50c9474302631a99a39
= <=>
2023-11-16 10:17:39 -0500

    practica08

---
+++ S08_12/src/s08_12/S08_12.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package s08_12;
+
+import s08_12.pq1.class1;
+import s08_12.pq1.class2;
+import s08_12.pq2.class3;
+import s08_12.pq2.class4;
+/**
+ *
+ * @author cga20
+ */
+public class S08_12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
